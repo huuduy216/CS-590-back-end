d074e0bab79793a5bd28ffe8ca03175d4fe25c54
jijiaqi16 <1160117897j@gmail.com>
2021-11-06 16:20:41 -0400

    11/6/2021

--- src/main/java/com/example/spacecode/controller/TestController.java
+++ src/main/java/com/example/spacecode/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.spacecode.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 public class TestController {
